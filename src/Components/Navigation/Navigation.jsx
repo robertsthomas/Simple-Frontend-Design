566f23bf80ab3e03113764cdfee8c5768328f11b
Thomas Roberts <robertsthomasd@gmail.com>
2019-01-22 17:04:32 -0800

    starting over

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Navigation = () => (
+
+	<nav className="navbar">
+		<a href="#" className="navbar-brand">
+			<ul className="navbar-nav">
+				<li className="nav-item"><a href="#" className="nav-link">Home</a></li>
+			</ul>
+		</a>
+	</nav>
+
+)
+
+export default Navigation;
